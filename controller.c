ed2bc87a5a24db4d2c6d90a28f98b3de972afdc7
Nikoly Cover <nikoly_cover@outlook.com>
2024-01-17 20:19:51 -0300

    feat: reading all afd

--- controller.c
+++ controller.c
@@ -7,16 +7,38 @@
 #include <stdlib.h>
 
 void start() {
-    char alphabet[MAX_LENGTH];
-    char states[MAX_LENGTH][MAX_STRING_LENGTH];
-    char final_states[MAX_LENGTH][MAX_STRING_LENGTH];
+    char * alphabet[MAX_LENGTH];
+    char * states[MAX_LENGTH];
+    char * final_states[MAX_LENGTH];
     Transition * transitions = NULL;//malloc(sizeof(Transition) * MAX_LENGTH);
-    int qtd_states;
+    int alphabet_size = 0, states_size = 0, final_states_size = 0, transitions_size = 0;
 
-    read_afd("afd.txt", alphabet, states, final_states, transitions, &qtd_states);
-    printf(alphabet);
-    printf("\nqtd: %d", qtd_states);
-    //printf("%s", states);
+    read_afd("afd.txt", alphabet, states, final_states, &transitions, &alphabet_size, &states_size, &final_states_size, &transitions_size);
+
+    printf("\nAlfabeto: ");
+    for (int i = 0; i < alphabet_size; i++) {
+        printf("%s ", alphabet[i]);
+    }
+
+    printf("\nEstados: ");
+    for (int i = 0; i < states_size; i++) {
+        printf("%s ", states[i]);
+    }
+
+    printf("\nFinais: ");
+    for (int i = 0; i < final_states_size; i++) {
+        printf("%s ", final_states[i]);
+    }
+
+    printf("\nTransições: %d\n", transitions_size);
+    for (int i = 0; i < transitions_size; i++) {
+        printf("(%s, %c) = %s\n", transitions[i].current_state, transitions[i].symbol, transitions[i].next_state);
+    }
+
+    for (int i = 0; i < transitions_size; i++) {
+        free(transitions[i].current_state);
+        free(transitions[i].next_state);
+    }
 
     free(transitions);
 }
--- service.c
+++ service.c
@@ -7,68 +7,59 @@
 #include <stdlib.h>
 #include "string.h"
 
-void removeCommas(char *str) {
-    int i, j = 0;
-    for (i = 0; str[i] != '\0'; i++) {
-        if (str[i] != ',') {
-            str[j++] = str[i];
+void getData(char *line, char **array, int *size) {
+    char *start = strchr(line, '{');
+    char *end = strchr(line, '}');
+
+    if (start != NULL && end != NULL) {
+        start++;  // Pular o '{'
+        *end = '\0';  // Substituir '}' por '\0' para terminar a string no local certo
+
+        char *token = strtok(start, ",");
+        while (token != NULL) {
+            if (strcmp(token, "")) {
+                array[*size] = strdup(token); // strdup cria uma cópia independente da string line
+                (*size)++;
+            }
+            token = strtok(NULL, ",");
         }
     }
-    str[j] = '\0';
 }
 
-void read_afd(const char * filename, char * alphabet, char ** states, char *** final_states, Transition ** transitions, int * qtd_states) {
+void read_afd(const char * filename, char ** alphabet, char ** states, char ** final_states, Transition ** transitions, int * alphabet_size, int * states_size, int * final_states_size, int * transitions_size) {
     FILE * file = fopen(filename, "r");
     if (file == NULL) {
         perror("Erro ao abrir o arquivo");
         exit(EXIT_FAILURE);
     }
+    char line[MAX_LENGTH];
 
-    //lendo alfabeto
-    fscanf(file, "alfabeto={%[^}]}", alphabet);
-    removeCommas(alphabet);
-    printf("aux: %s\n", alphabet);
+    while (fgets(line, MAX_LENGTH, file) != NULL) {
+        if (strstr(line, "alfabeto") != NULL) {
+            getData(line, alphabet, alphabet_size);
+        } else if (strstr(line, "estados") != NULL) {
+            getData(line, states, states_size);
+        } else if (strstr(line, "finais") != NULL) {
+            getData(line, final_states, final_states_size);
+        }
+        else {
+            // Linha de transição
+            if (*transitions_size == 0) {
+                *transitions = malloc(sizeof(Transition));
+            } else {
+                *transitions = realloc(*transitions, (*transitions_size + 1) * sizeof(Transition));
+            }
 
-    // Leitura dos estados
-    fscanf(file, "estados={");
-    *states = 0;
-    while (fscanf(file, "%s", states[*qtd_states]) == 1) {
-        removeCommas(states[*qtd_states]);
-        printf("oi: %s", states[*qtd_states]);
-        (*qtd_states)++;
+            char current_state[MAX_STRING_LENGTH], next_state[MAX_STRING_LENGTH];
+            char symbol;
+            sscanf(line, "(%[^,],%c)= %s", current_state, &symbol, next_state);
+
+            (*transitions)[*transitions_size].current_state = strdup(current_state);
+            (*transitions)[*transitions_size].symbol = symbol;
+            (*transitions)[*transitions_size].next_state = strdup(next_state);
+            (*transitions_size)++;
+        }
     }
-
-/*
-    // Leitura dos estados
-    fscanf(arq, "estados={");
-    int i = 0;
-    char estado[20];  // Ajuste o tamanho conforme necessário
-    while (fscanf(arq, "%[^,}]%*[,}] ", estado) == 1) {
-        *estados = realloc(*estados, (i + 1) * sizeof(char *));
-        (*estados)[i] = strdup(estado);
-        i++;
-    }
-
-    // Leitura dos estados finais
-    fscanf(arq, "finais={");
-    i = 0;
-    while (fscanf(arq, "%[^,}]%*[,}] ", estado) == 1) {
-        *estados_finais = realloc(*estados_finais, (i + 1) * sizeof(char *));
-        (*estados_finais)[i] = strdup(estado);
-        i++;
-    }
-
-    // Leitura das transições
-    i = 0;
-    *transicoes = malloc(sizeof(Transition));
-    while (fscanf(arq, "(%[^,],%c)= %s ", estado, &(*transicoes)[i].symbol, estado) == 3) {
-        (*transicoes)[i].current_state = strdup(estado);
-        (*transicoes)[i].next_state = strdup(estado);
-        i++;
-        *transicoes = realloc(*transicoes, (i + 1) * sizeof(Transition));
-    }
-
-    *num_transicoes = i;*/
 
     fclose(file);
 }
